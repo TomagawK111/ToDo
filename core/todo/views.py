c5dea7c4b52f061e58acd2141fb4d35bd114003f
Tumer124 <egor.bugaev2015@yandex.ru>
2023-04-17 21:28:36 +0700

    project created

---
+++ core/todo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def main_page(request):
+	return HttpResponse("Hello world")
+
+
+def about_page(request):
+	return HttpResponse("Buy Buy World")
